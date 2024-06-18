d019e8f4ae1e5b1f6eb72a564e6ca37fc57a6f9d
GmeduoyeClaymore <31854059+GmeduoyeClaymore@users.noreply.github.com>
2024-06-18 09:48:04 +0100

    Treat

--- src/trait_object.rs
+++ src/trait_object.rs
@@ -240,9 +240,11 @@ pub fn generate_dotnet_wrapper_objects_for_trait<'a>(
                         let ty = &pat_type.ty;
                         if is_primitive(ty) {
                             Some(quote! {
-                                let r1 = #pat;
-                                log::info!("Primitive Param: {} {:?}",stringify!(#pat), r1);
-                                r1
+                                {
+                                    let r1 = #pat;
+                                    log::info!("Primitive Param: {} {:?}",stringify!(#pat), r1);
+                                    r1
+                                }
                             })
                         }
                         else if is_string(ty) {
@@ -251,7 +253,7 @@ pub fn generate_dotnet_wrapper_objects_for_trait<'a>(
                                 let c_str = std::ffi::CStr::from_ptr(#pat);
                                 let string_unwrapped = c_str.to_str().expect("Failed to get string from c string");
                                 let r1 = string_unwrapped.to_string();
-                                log::info!("String Param: {} {:?}",stringify!(#pat), r1);
+                                //log::info!("String Param: {} {:?}",stringify!(#pat), r1);
                                 r1
                             }
                         })
@@ -261,7 +263,7 @@ pub fn generate_dotnet_wrapper_objects_for_trait<'a>(
                                 let c_str = std::ffi::CStr::from_ptr(#pat);
                                 let string_unwrapped = c_str.to_str().expect("Failed to get string from c string");
                                 let r1 = serde_json::from_str(&string_unwrapped).expect(format!("Failed to serialize param {} from string {}", stringify!(#pat), string_unwrapped).as_str());
-                                log::info!("Typed Param: {} {:?}",stringify!(#pat), r1);
+                                //log::info!("Typed Param: {} {:?}",stringify!(#pat), r1);
                                 r1
                             }
                         })
