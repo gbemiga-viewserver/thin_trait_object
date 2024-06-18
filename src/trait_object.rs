be5b36266ee9b1e6cb8a2445275a3d5736b5727b
GmeduoyeClaymore <31854059+GmeduoyeClaymore@users.noreply.github.com>
2024-06-18 07:46:57 +0100

    Treat

--- src/trait_object.rs
+++ src/trait_object.rs
@@ -272,8 +272,9 @@ pub fn generate_dotnet_wrapper_objects_for_trait<'a>(
                         quote! { result }
                     } else {
                         quote! {
+                        log::info!("Unserialized Result: {:?}", result);
                         let result_str = serde_json::to_string(&result).expect("Failed to serialize result");
-                        log::info!("Result: {}", result_str);
+                        log::info!("Serialized Result: {}", result_str);
                         std::ffi::CString::new(result_str).expect("Failed to get typed result from string result").into_raw()
                     }
                     }
